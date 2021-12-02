dc3d11e67b2c074e4d801a5f0450ae78c828bbd2
Nick DeJesus <ndejesus1227@gmail.com>
2021-12-02 13:41:55 -0500

    chore: update after rename

--- package/src/index.ts
+++ package/src/index.ts
@@ -2,7 +2,7 @@ export type { SetPreviewDataConfig } from "./setPreviewData";
 export type { EnableAutoPreviewsConfig } from "./enableAutoPreviews";
 export { setPreviewData } from "./setPreviewData";
 export { exitPreview } from "./exitPreview";
-export { PrismicPreview } from "./preview";
+export { PrismicPreview } from "./PrismicPreview";
 export { enableAutoPreviews } from "./enableAutoPreviews";
 export { redirectToPreviewURL } from "./redirectToPreviewURL";
 export type { NextContextLike, CreateClientConfig } from "./types";
