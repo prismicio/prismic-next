fc13319209e226a6e03e19b1d431fbc76777622b
Nick DeJesus <ndejesus1227@gmail.com>
2021-12-07 10:56:01 -0500

    chore: export ExitPreviewParams

--- package/src/index.ts
+++ package/src/index.ts
@@ -1,7 +1,7 @@
 export type { SetPreviewDataConfig } from "./setPreviewData";
 export type { EnableAutoPreviewsConfig } from "./enableAutoPreviews";
 export { setPreviewData } from "./setPreviewData";
-export { exitPreview } from "./exitPreview";
+export { exitPreview, ExitPreviewParams } from "./exitPreview";
 export { PrismicPreview } from "./PrismicPreview";
 export { enableAutoPreviews } from "./enableAutoPreviews";
 export { redirectToPreviewURL } from "./redirectToPreviewURL";
