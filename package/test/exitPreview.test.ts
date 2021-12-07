a9d473b9f90c1414c0a2512085e0cd43324c8323
Nick DeJesus <ndejesus1227@gmail.com>
2021-12-07 10:55:48 -0500

    chore: fix imports and remove server

--- package/test/exitPreview.test.ts
+++ package/test/exitPreview.test.ts
@@ -1,14 +1,7 @@
 import test from "ava";
 import * as sinon from "sinon";
-import * as msw from "msw";
-import * as mswNode from "msw/node";
-
-import { exitPreview } from "../src/exitPreview";
-import { ExitPreviewParams } from "../src/exitPreview";
-
-const server = mswNode.setupServer();
-test.before(() => server.listen({ onUnhandledRequest: "error" }));
-test.after(() => server.close());
+import { exitPreview } from "../src";
+import { ExitPreviewParams } from "../src";
 
 test("exitPreview runs clearPreviewData", async (t) => {
 	// const res: NextApiResponse = { clearPreviewData: sinon.stub() };
@@ -18,13 +11,8 @@ test("exitPreview runs clearPreviewData", async (t) => {
 			clearPreviewData: sinon.stub(),
 		},
 	};
-	server.use(
-		msw.rest.get("qwerty", (req, res, ctx) => {
-			return res(ctx.json({}));
-		}),
-	);
 
-	await exitPreview(config);
+	exitPreview(config);
 
 	t.true((config.res.clearPreviewData as sinon.SinonStub).calledWith());
 });
