4cf31adb568cb441d0b28c19a775b048a013e234
Nick DeJesus <ndejesus1227@gmail.com>
2021-12-07 18:47:45 -0500

    refactor to fix typescript error

--- package/test/__testutils__/test-utils.tsx
+++ package/test/__testutils__/test-utils.tsx
@@ -5,7 +5,11 @@ import { PrismicPreview } from "../../src";
 const customRender = (
 	ui: ReactElement,
 	options?: Omit<RenderOptions, "wrapper">,
-) => render(ui, { wrapper: PrismicPreview, ...options });
+) =>
+	render(ui, {
+		wrapper: (props) => <PrismicPreview {...props} repositoryName="qwerty" />,
+		...options,
+	});
 
 export * from "@testing-library/react";
 export { customRender as render };
