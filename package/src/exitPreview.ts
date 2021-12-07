000c89bf253f70b6cd2be04b55df27c565ee0829
Nick DeJesus <ndejesus1227@gmail.com>
2021-12-07 18:47:02 -0500

    chore: refactor to use req

--- package/src/exitPreview.ts
+++ package/src/exitPreview.ts
@@ -1,9 +1,14 @@
-import { NextApiResponse } from "next";
+import { NextApiResponse, NextApiRequest } from "next";
 
 export type ExitPreviewParams = {
 	res: {
 		clearPreviewData: NextApiResponse["clearPreviewData"];
 		redirect: NextApiResponse["redirect"];
+	};
+	req: {
+		headers: {
+			referer?: NextApiRequest["headers"]["referer"];
+		};
 	};
 };
 
@@ -13,11 +18,18 @@ export type ExitPreviewParams = {
  * @description Exits preview mode
  */
 export function exitPreview(config: ExitPreviewParams) {
+	const { req } = config;
 	// Exit the current user from "Preview Mode". This function accepts no args.
 	config.res.clearPreviewData();
 
-	// Redirect the user back to the index page.
+	if (req.headers.referer) {
+		const url = new URL(req.headers.referer);
+
+		if (url.pathname !== "/api/exit-preview") {
+			// Redirect the user to the referrer page.
+			config.res.redirect(req.headers.referer);
+		}
+	}
+
 	config.res.redirect("/");
-	// config.res.writeHead(307, { Location: "/" });
-	// config.res.end();
 }
