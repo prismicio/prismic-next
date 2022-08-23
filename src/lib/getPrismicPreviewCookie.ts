7a54b644d009f547b58de7d842d42b20544932f3
Angelo Ashmore <angeloashmore@users.noreply.github.com>
2022-08-22 14:59:40 -1000

    refactor: simplify getCookie to getPrismicPreviewCookie

--- src/lib/getPrismicPreviewCookie.ts
+++ src/lib/getPrismicPreviewCookie.ts
@@ -1,3 +1,5 @@
+import * as prismic from "@prismicio/client";
+
 const readValue = (value: string): string => {
 	return value.replace(/%3B/g, ";");
 };
@@ -5,25 +7,26 @@ const readValue = (value: string): string => {
 /**
  * Returns the value of a cookie from a given cookie store.
  *
- * @param name - Name of the cookie.
  * @param cookieJar - The stringified cookie store from which to read the cookie.
  *
  * @returns The value of the cookie, if it exists.
  */
-export const getCookie = (
-	name: string,
+export const getPrismicPreviewCookie = (
 	cookieJar: string,
 ): string | undefined => {
 	const cookies = cookieJar.split("; ");
 
+	let value: string | undefined;
+
 	for (const cookie of cookies) {
 		const parts = cookie.split("=");
-		const thisName = readValue(parts[0]).replace(/%3D/g, "=");
+		const name = readValue(parts[0]).replace(/%3D/g, "=");
 
-		if (thisName === name) {
-			const value = parts.slice(1).join("=");
-
-			return readValue(value);
+		if (name === prismic.cookie.preview) {
+			value = readValue(parts.slice(1).join("="));
+			continue;
 		}
 	}
+
+	return value;
 };
