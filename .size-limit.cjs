ad2b9d85986bacebeff3f94a9c7f58399e177873
Angelo Ashmore <angeloashmore@users.noreply.github.com>
2022-09-08 14:27:53 -1000

    chore: update Size Limit config to use `exports` values

--- .size-limit.cjs
+++ .size-limit.cjs
@@ -1,1 +1,17 @@
-module.exports = [{ path: "./dist/index.mjs" }];
+const pkg = require("./package.json");
+
+module.exports = [
+	...new Set(
+		Object.values(pkg.exports).flatMap((exportTypes) => {
+			if (typeof exportTypes === "string") {
+				return exportTypes;
+			} else {
+				return Object.values(exportTypes);
+			}
+		}),
+	),
+]
+	.filter(Boolean)
+	.map((path) => {
+		return { path };
+	});
