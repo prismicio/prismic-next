faf568c3525c2a77ec1ae7b4054a7a8585fca6d5
Angelo Ashmore <angeloashmore@users.noreply.github.com>
2023-04-27 08:15:51 -1000

    refactor: use more accurate name for unsupported error builder

--- src/react-server/unsupported.ts
+++ src/react-server/unsupported.ts
@@ -1,26 +1,26 @@
 // Exports in this file are unsupported in the react-server environment.
 // All environments should export the same functions to maintian API compatability.
 
-function buildErrorMessage(fnName: string) {
+function buildIncompatiblePreviewModeError(fnName: string) {
 	return `${fnName} is currently incompatible with App Router. Preview Mode is not supported in App Router at this time. Please remove all uses of ${fnName}.`;
 }
 
 export function PrismicPreview(): JSX.Element {
-	throw new Error(buildErrorMessage("<PrismicPreview>"));
+	throw new Error(buildIncompatiblePreviewModeError("<PrismicPreview>"));
 }
 
 export function enableAutoPreviews() {
-	throw new Error(buildErrorMessage("enableAutoPreviews()"));
+	throw new Error(buildIncompatiblePreviewModeError("enableAutoPreviews()"));
 }
 
 export function exitPreview() {
-	throw new Error(buildErrorMessage("exitPreview()"));
+	throw new Error(buildIncompatiblePreviewModeError("exitPreview()"));
 }
 
 export function redirectToPreviewURL() {
-	throw new Error(buildErrorMessage("redirectToPreviewURL()"));
+	throw new Error(buildIncompatiblePreviewModeError("redirectToPreviewURL()"));
 }
 
 export function setPreviewData() {
-	throw new Error(buildErrorMessage("setPreviewData()"));
+	throw new Error(buildIncompatiblePreviewModeError("setPreviewData()"));
 }
