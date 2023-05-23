25027024a23141a33a0b65ea9d25594f03f75966
Angelo Ashmore <angeloashmore@users.noreply.github.com>
2023-05-22 14:10:40 -1000

    chore: remove example project

--- example/pages/api/exit-preview.ts
+++
@@ -1,9 +0,0 @@
-import { NextApiRequest, NextApiResponse } from "next";
-import { exitPreview } from "@prismicio/next";
-
-export default function handler(
-	req: NextApiRequest,
-	res: NextApiResponse,
-): void {
-	exitPreview({ req, res });
-}
