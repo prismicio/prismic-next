9777ff052255a3a2d928f77b644d9ba03ded9ae6
Angelo Ashmore <angeloashmore@users.noreply.github.com>
2025-01-09 13:46:48 -1000

    test: add `experimental.outputFileTracingRoot` to Next.js 14 projects

--- e2e-projects/14-pages-router/next.config.mjs
+++ e2e-projects/14-pages-router/next.config.mjs
@@ -1,7 +1,12 @@
 // @ts-check
 
+import { fileURLToPath } from "node:url";
+
 /** @type {import("next").NextConfig} */
 const nextConfig = {
+	experimental: {
+		outputFileTracingRoot: fileURLToPath(new URL("../..", import.meta.url)),
+	},
 	images: {
 		remotePatterns: [{ hostname: "images.prismic.io" }],
 	},
