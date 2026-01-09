1fd50f8f7982df9d027a0e04150a2ccc0ba43f9e
Angelo Ashmore <angeloashmore@users.noreply.github.com>
2026-01-08 15:32:57 -1000

    refactor: simplify

--- src/setGlobalConfig.ts
+++ src/setGlobalConfig.ts
@@ -1,20 +1,6 @@
-import { HTMLAttributeAnchorTarget, ReactNode } from "react";
 import Link from "next/link";
 
-const globalConfigKey = Symbol.for("@prismicio/react/config");
-
-type GlobalThis = {
-	[globalConfigKey]?: {
-		internalLinkComponent?: React.ComponentType<{
-			href: string;
-			target?: HTMLAttributeAnchorTarget;
-			rel?: string;
-			children?: ReactNode;
-		}>;
-	};
-};
-
-(globalThis as GlobalThis)[globalConfigKey] = {
-	...(globalThis as GlobalThis)[globalConfigKey],
-	internalLinkComponent: Link,
-};
+// @ts-expect-error - Property doesn't exist on global
+globalThis[
+	Symbol.for("@prismicio/react/PrismicLink/defaultInternalComponent")
+] = Link;
